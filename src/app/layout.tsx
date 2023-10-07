42f19b6dee0634415c9b857cd8779debe68dc991
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-10-07 09:58:19 +0700

    chore: install mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import '@mantine/core/styles.css';
 import '~/lib/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -33,7 +35,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
