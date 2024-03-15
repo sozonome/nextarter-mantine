2ab425fb3ef21df3549e1ef0aee37fab986dd63f
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-03-15 23:00:56 +0700

    fix: adjust themetoggle

--- src/lib/layout/components/ThemeToggle.tsx
+++ src/lib/layout/components/ThemeToggle.tsx
@@ -1,25 +1,27 @@
-'use client';
+"use client";
 
-import { useMantineColorScheme } from '@mantine/core';
-import { Within } from '@theme-toggles/react';
-import '@theme-toggles/react/css/Within.css';
+import { useMantineColorScheme } from "@mantine/core";
+import { Within } from "@theme-toggles/react";
+import "@theme-toggles/react/css/Within.css";
 
-import styles from './ThemeToggle.module.css';
+import styles from "./ThemeToggle.module.css";
 
 const ThemeToggle = () => {
   const { setColorScheme, colorScheme } = useMantineColorScheme({
     keepTransitions: true,
   });
   const toggle = () =>
-    setColorScheme(colorScheme === 'light' ? 'dark' : 'light');
+    setColorScheme(colorScheme === "light" ? "dark" : "light");
 
   return (
     <Within
-      toggled={colorScheme === 'light'}
+      toggled={colorScheme === "light"}
       toggle={toggle}
       duration={250}
-      className={styles['theme-toggle']}
-      placeholder={'' as unknown}
+      className={styles["theme-toggle"]}
+      placeholder={"" as unknown}
+      onPointerEnterCapture={() => {}}
+      onPointerLeaveCapture={() => {}}
     />
   );
 };
